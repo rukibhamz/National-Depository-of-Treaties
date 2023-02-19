03826729362f2ec021544e9dc1ed045bb63894f1
traj3ctory <davidbenard.bd@gmail.com>
2023-02-19 08:13:28 +0100

    feat: remove the executable files and update the dashboard view

--- staff/pages_staff_new_treaty.php
+++ staff/pages_staff_new_treaty.php
@@ -82,7 +82,7 @@ if (isset($_POST['add_treaty']) && $user->acc_status == 'Active') {
             $b_file = $_FILES["b_file"]["name"];
 
             if (!empty($b_file)) {
-                move_uploaded_file($_FILES["b_file"]["tmp_name"], "assets/magazines/" . $_FILES["b_file"]["name"]);
+                move_uploaded_file($_FILES["b_file"]["tmp_name"], ".." . DIRECTORY_SEPARATOR . "sudo" . DIRECTORY_SEPARATOR . "assets" . DIRECTORY_SEPARATOR . "magazines" . DIRECTORY_SEPARATOR . $_FILES["b_file"]["name"]);
                 // move_uploaded_file($_FILES["b_file"]["tmp_name"], "assets" . DIRECTORY_SEPARATOR . "magazines" . DIRECTORY_SEPARATOR . $_FILES["b_file"]["name"]);
             } else {
                 $err = "Please select a file to upload.";
